3946170ab437fdd6a4e4b608009a6709791ba8b9
wcampbell <wcampbell1995@gmail.com>
2022-03-21 23:10:09 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# karkin-os
+
+```
+> cargo install bootimage
+> rustup component add llvm-tools-preview
+> cargo r --release
+```
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use vga_buffer::welcome;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    welcome();
+    loop {}
+}
